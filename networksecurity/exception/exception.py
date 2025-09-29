7e1f6a7897791bdc6f7342e217c4917a6ff7730d
Bhupinder Kaur <bhupinderkaur027@gmail.com>
2025-09-28 21:28:38 -0400

    dataset

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -13,8 +13,6 @@ class NetworkSecurityException(Exception):
         return "Error occured in python script name [{0}] line number [{1}] error message[{2}]"
         self.file_name,self.lineno,str(self.error_message)
         
-        
-    
 if __name__=='__main__':
     try:
         logger.logging.info("Enter the try block")
